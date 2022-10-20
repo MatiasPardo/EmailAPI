f7c1cab8eb6a526999d84ce8cb7d2be6733822d6
Matias <pardmatias@est.frba.utn.edu.ar>
2022-10-20 10:52:36 -0300

    attachament optional and update pom

--- src/main/java/com/email/model/EmailService.java
+++ src/main/java/com/email/model/EmailService.java
@@ -1,6 +1,8 @@
 package com.email.model;
 
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -15,16 +17,17 @@ public class EmailService {
 
         Attachment[] attachments = null;
         if(email.getFilename() != null && email.getSchema() != null){
-            String path = "C:/ConfigFEArg/" + email.getSchema() + "/Email/" + email.getFilename();
+            String path = "C:/MparDOS/" + email.getSchema() + "/" + email.getFilename();
             File file = new File(path);
-            if(!file.exists())
-                throw new Exception("No existe el archivo");
-
-            attachments = new Attachment[1];
-            Attachment attach = new Attachment(file.getName(), file);
-            attachments[0] = attach; 	
-        }else{
-            
+            if(!file.exists()){
+                Logger logger = Logger.getLogger("Email");
+                logger.setLevel(Level.INFO);
+                logger.info("No existe el archivo");
+            }else{
+                attachments = new Attachment[1];
+                Attachment attach = new Attachment(file.getName(), file);
+                attachments[0] = attach;
+            }
         }
 
         Email.sendEmail(email.getSmtpHost(), email.getSmtpPort(), email.getMailUser(), email.getMailUserPassword(),
